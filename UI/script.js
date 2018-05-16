328d67775e179bc44c792f6b7eedb5d9065c933b
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-16 03:41:59 -1200

    ft(user-make-request-modal): implements the user request modal - I created the modal using html - The users inputs are stored in a form field - I style the modals with scss - I use js for toggling and modal effects [finishes #157573918]

--- UI/script.js
+++ UI/script.js
@@ -1,11 +1,21 @@
-$(document). ready(function() {
-    $(".menu-icon").on("click", function(){
-        $("nav .menu ul").toggleClass("showing");
-    })
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav .menu ul").toggleClass("showing");
+  });
+  //add request
+  $("#make_request").on("click", function() {
+    $("#modal_make_request").fadeIn("slow");
+    $("#make_request_content").fadeIn("slow");
+  });
+  // close
+  $(".close").on("click", function() {
+    $("#modal_make_request").fadeOut("slow");
+    $("#make_request_content").fadeOut("slow");
+  });
 });
 //dashboard menu toggle
-$(document). ready(function() {
-    $(".menu-icon").on("click", function(){
-        $(".sidebar").toggleClass("active");
-    })
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $(".sidebar").toggleClass("active");
+  });
 });
