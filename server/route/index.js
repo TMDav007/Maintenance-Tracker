42771acfefb924ac005ee43d1b3676e46aefc635
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-16 14:57:15 -1200

    ch(serverside-setup): sets up the serverside - I run dpm init, - I install express,body-parser,nodemon,chai,mocha,chai-http - I install babel-core,babel-cli,babel-preset-env,eslint,eslint-config-aibnb,eslint-plugin-import i set up the server to run at localhost://8000 [finishes #157645402]

---
+++ server/route/index.js
@@ -0,0 +1,14 @@
+
+const routes = (app) => {
+  app.get('', (req, res) =>
+    res.status(200).send({
+      message: 'Welcome to Maintenance-Tracker App, add "/api/v1/" to use the api'
+    }));
+
+  app.get('/api/v1/', (req, res) =>
+    res.status(200).send({
+      message: 'to view api, add /request'
+    }));
+};
+
+export default routes;
