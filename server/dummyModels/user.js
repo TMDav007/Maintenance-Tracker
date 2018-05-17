d0d3dfa6848329d5d50112f7f8f70e56cab2f79a
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-16 20:30:29 -1200

    ft(user-loggedin): implements the controller function to get all users request - I created a user dummmy data that has users as an admin and as a user - I created a requestController class with the method getAllRequests to get all users request - I updated the route index with the GET method for getAllRequests - I write the test and tested with mocha and coverage - I add travis file - I add travis,code climate, coveralls badges [finishes #157645007]

---
+++ server/dummyModels/user.js
@@ -0,0 +1,16 @@
+const user = [
+  {
+    id: 1,
+    name: 'Ajao',
+    password: 'aj',
+    role: 'admin'
+  },
+  {
+    id: 2,
+    name: 'Jacobs',
+    password: 'aj',
+    role: 'user'
+  }
+];
+
+export default user;
