93a6fb663a61baac69688a16bff4ce273d161b91
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-05-21 20:59:41 +0100

    ch(setup-db): set up the db - I install the pg dependencyto connect it with postgres - I add the db details in the config file [finishes #157751261]

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT
+};
+
+export default config;
